2eb305d1ac5a8668327eab396d31595e356e87d3
Hussain Mahammad Habib <habibsain25@gmail.com>
2025-08-08 18:48:42 +0000

    added insertion sort algorithm

--- insertion_sort.c
+++ insertion_sort.c
@@ -2,7 +2,18 @@
 
 void sort(int* arr, int num)
 {
+    for (int i = 1; i < 5; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
 
+        //insert arr[i] to sorted array before it
+        for (j = i - 1; j >= 0 && arr[j] > key; j--)
+        {
+            arr[j + 1] = arr[j];
+        }
+        arr[j + 1] = key;
+    }
 }
 
 void print_arr(int* arr, int num)
@@ -16,11 +27,15 @@ void print_arr(int* arr, int num)
 
 int main()
 {
-    int num = 5
-    int arr[num] = {2, 4, 1, 5, 3}
+    int num = 5;
+    int arr[] = {2, 4, 1, 5, 3};
+
     //print array
     print_arr(arr, num);
-    //sort(arr, num)
+    sort(arr, num);
 
     //print sorted array
+    print_arr(arr, num);
+
+    return 0;
 }
