de4c884d534bda74c5a02e151394b9e30dcbebc3
Hussain Mahammad Habib <habibsain25@gmail.com>
2025-08-08 18:27:18 +0000

    initialised and added printinng function

---
+++ insertion_sort.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void sort(int* arr, int num)
+{
+
+}
+
+void print_arr(int* arr, int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int num = 5
+    int arr[num] = {2, 4, 1, 5, 3}
+    //print array
+    print_arr(arr, num);
+    //sort(arr, num)
+
+    //print sorted array
+}
