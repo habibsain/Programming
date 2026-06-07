3d89f17c19beeb7e5b19da25e179a0a73a1e0e74
Hussain Mahammad Habib <habibsain25@gmail.com>
2026-06-07 18:07:52 +0000

    completed partition and quicksort

--- DSA/src/algorithms.py
+++ DSA/src/algorithms.py
@@ -6,48 +6,45 @@
 
 #-------helper functions---------#
 
-def swap(x: any, y: any)->None:
-    temp: any = x
-    x = y
-    y = temp
-
 #places the last element to it's correct position    
-def partition(num_list: list[int | float])-> list[int | float]:
-    p = 0
-    r = len(num_list) - 1
-    i = p - 1
-    j = p
-    x = num_list[r]
+def partition(num_list: list[int | float], low: int, high: int):
+    r = high
+    i = low - 1
+    j = low
+    x = num_list[high]
     while(j < r):
         if(num_list[j] < x):
             i = i + 1
-            swap(num_list[i], num_list[j])
+            num_list[i], num_list[j] = num_list[j], num_list[i]
         j = j + 1
-    swap(num_list[i + 1], num_list[r])
+    num_list[i + 1], num_list[r] = num_list[r], num_list[i+1]
     return i + 1
 
-class Sort():
-    def __init__(self, num_list: list[int | float]):
-        self.num_list = num_list
-        self.start = 0
-        self.length = len(num_list)
-        self.end = len(num_list) - 1
+# class Sort():
+#     def __init__(self, num_list: list[int | float]):
+#         self.num_list = num_list
+#         self.start = 0
+#         self.length = len(num_list)
+#         self.end = len(num_list) - 1
 
-    def quicksort(p, r):
+#     def quicksort(p, r):
+#         pass
         
 
 
-def quicksort(num_list: list[int | float], r: int, p: int = 0,)-> list[int | float]:
-    p = 0
-    r = len(num_list) - 1
-    if(p < r):
-        q = partition(num_list)
-    return [2, 4, 8, 1, 9, 10, 1.3, 3]
+def quicksort(num_list: list[int | float], low: int , high: int)-> None:
+    # p = 0
+    # r = len(num_list) - 1
+    if(low < high):
+        q = partition(num_list, low, high)
+        quicksort(num_list, low, q - 1)
+        quicksort(num_list, q + 1, high)
+    # return [2, 4, 8, 1, 9, 10, 1.3, 3]
 
 
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     numList = [2, 4, 8, 1, 9, 10, 1.3, 3]
-    x = partition(numList)
+    x = partition(numList, 0, len(numList) - 1)
     print(x)
--- DSA/test_dsa.py
+++ DSA/test_dsa.py
@@ -12,15 +12,23 @@ def test_hello():
 numList1 = [2, 4, 8, 1, 9, 10, 1.3, 3]
 numList2 = [2, 4, 8, 1, 9, 10, 1.3, 5]
 
+l1 = 0
+r1 = len(numList1) - 1
+
+l2 = 0
+r2 = len(numList2) - 1
+
 #[2, 1, 1.3, 3, 4, 8, 9, 10]
 
 def test_partition():
-    assert algorithms.partition(num_list=numList1) == 3
-    assert algorithms.partition(num_list=numList2) == 4
+    assert algorithms.partition(numList1, l1, r1) == 3
+    assert algorithms.partition(numList2, l2, r2) == 4
 
 def test_quicksort():
-    assert algorithms.quicksort(numList1) == [1, 1.3, 2, 3, 4, 8, 9, 10]
-    
+    algorithms.quicksort(numList1, 0, len(numList1) - 1)
+    assert  numList1 == [1, 1.3, 2, 3, 4, 8, 9, 10]
+    algorithms.quicksort(numList2, 0, len(numList2) - 1)
+    assert  numList2 == [1, 1.3, 2, 4, 5, 8, 9, 10]
 
 
 # if __name__ == "main":
