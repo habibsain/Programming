749f5cc2f51a28009d350971bd2453953641d487
Hussain Mahammad Habib <habibsain25@gmail.com>
2026-06-08 13:28:25 +0000

    added swap_up and swap_down

--- DSA/src/datastructures.py
+++ DSA/src/datastructures.py
@@ -16,9 +16,41 @@
 #Hash table
 ##with-> separate chaining, linear probing, quadratic probing, double hashing
 
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def insert_node(self):
+        #Insert at end and until it reaches proper position swap_up
+        pass
+
+    def swap_up(self, arr: list[int | float], index):
+        #If parent is smaller than child swap
+        parent = (index - 1) // 2
+        if arr[parent] < arr[index]:
+            arr[parent], arr[index] = arr[index], arr[parent]
+            self.swap_up(arr, index)
+
+    def swap_down(self, arr: list[int | float], index):
+        #If parent is smaller than child swap
+        max = index
+        if arr[2 * index + 1] > arr[max]:
+            max = 2 * index + 1
+
+        if arr[2 * index + 2] > arr[max]:
+            max = 2 * index + 2
+
+
+        if max != index
+            arr[max], arr[index] = arr[index], arr[max]
+            self.swap_down(arr, index)
+
+    def extract_max(self):
+        #return the root node 
+        #replace the root node with last node and swap down
 
 
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     pass
