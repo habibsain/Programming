a5832532b6eaf2f1e172ecf3c1f3a38f8f4ca7b9
Hussain Mahammad Habib <habibsain25@gmail.com>
2026-06-08 18:25:45 +0000

    max_heap and min heap completed and passed al tests

--- DSA/src/datastructures.py
+++ DSA/src/datastructures.py
@@ -20,37 +20,101 @@ class MaxHeap:
     def __init__(self):
         self.heap = []
 
-    def insert_node(self):
-        #Insert at end and until it reaches proper position swap_up
-        pass
-
-    def swap_up(self, arr: list[int | float], index):
+    def swap_up(self, index):
         #If parent is smaller than child swap
+        #arr = self.heap
         parent = (index - 1) // 2
-        if arr[parent] < arr[index]:
-            arr[parent], arr[index] = arr[index], arr[parent]
-            self.swap_up(arr, index)
+        if parent < 0:
+            return
+        if self.heap[parent] < self.heap[index]:
+            self.heap[parent], self.heap[index] = self.heap[index], self.heap[parent]
+            #print(self.heap)
+            self.swap_up(parent)
 
-    def swap_down(self, arr: list[int | float], index):
+    def swap_down(self, index):
         #If parent is smaller than child swap
+        arr = self.heap
+        l = len(arr)
         max = index
-        if arr[2 * index + 1] > arr[max]:
-            max = 2 * index + 1
+        child1 = 2 * index + 1
+        child2 = 2 * index + 2
+        if child1 < l and arr[child1] > arr[max]:
+            max = child1
 
-        if arr[2 * index + 2] > arr[max]:
-            max = 2 * index + 2
+        if child2 < l and arr[child2] > arr[max]:
+            max = child2
 
-
-        if max != index
+        if max != index:
             arr[max], arr[index] = arr[index], arr[max]
-            self.swap_down(arr, index)
+            self.swap_down(max)
+
+    def insert_node(self, val):
+        #Insert at end and until it reaches proper position swap_up
+        self.heap.append(val)
+        i = len(self.heap) - 1
+        self.swap_up(i)
 
     def extract_max(self):
         #return the root node 
         #replace the root node with last node and swap down
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self.swap_down(0)
 
+        return root
+    
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def swap_up(self, index):
+        #If parent is greater than child swap
+        #arr = self.heap
+        parent = (index - 1) // 2
+        if parent < 0:
+            return
+        if self.heap[parent] > self.heap[index]:
+            self.heap[parent], self.heap[index] = self.heap[index], self.heap[parent]
+            #print(self.heap)
+            self.swap_up(parent)
+
+    def swap_down(self, index):
+        #If parent is smaller than child swap
+        arr = self.heap
+        l = len(arr)
+        min = index
+        child1 = 2 * index + 1
+        child2 = 2 * index + 2
+        if child1 < l and arr[child1] < arr[min]:
+            min = child1
+
+        if child2 < l and arr[child2] < arr[min]:
+            min = child2
+
+        if min != index:
+            arr[min], arr[index] = arr[index], arr[min]
+            self.swap_down(min)
+
+    def insert_node(self, val):
+        #Insert at end and until it reaches proper position swap_up
+        self.heap.append(val)
+        i = len(self.heap) - 1
+        self.swap_up(i)
+
+    def extract_min(self):
+        #return the root node 
+        #replace the root node with last node and swap down
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self.swap_down(0)
+
+        return root
 
 
 
 if __name__ == "__main__":
-    pass
+    numList1 = [2, 4, 8, 1, 9, 10, 1.3, 3]
+    max_hp = MaxHeap()
+    for item in numList1:
+        max_hp.insert_node(item)
+        print(max_hp.heap)
--- DSA/test_dsa.py
+++ DSA/test_dsa.py
@@ -1,5 +1,5 @@
 #import pytest as pt
-from src import algorithms
+from src import algorithms, datastructures
 def hello(x: str):
     y = x.lower()
     if y == "hello":
@@ -29,6 +29,26 @@ def test_quicksort():
     assert  numList1 == [1, 1.3, 2, 3, 4, 8, 9, 10]
     algorithms.quicksort(numList2, 0, len(numList2) - 1)
     assert  numList2 == [1, 1.3, 2, 4, 5, 8, 9, 10]
+
+def test_maxHeap():
+    hp = datastructures.MaxHeap()
+    for item in numList1:
+        hp.insert_node(item)
+
+    assert hp.extract_max() == 10
+    assert hp.extract_max() == 9
+    assert hp.extract_max() == 8
+    assert hp.extract_max() == 4
+
+def test_minHeap():
+    hp1 = datastructures.MinHeap()
+    for item in numList1:
+        hp1.insert_node(item)
+
+    assert hp1.extract_min() == 1
+    assert hp1.extract_min() == 1.3
+    assert hp1.extract_min() == 2
+    assert hp1.extract_min() == 3
 
 
 # if __name__ == "main":
